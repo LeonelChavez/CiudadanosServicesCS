be6b44691533821adc3854ac3913115105d608d9
Jack Leo <leonel.chavez@me.com>
2025-10-16 12:18:16 -0600

    Add initial migration for TB_NombresPropios table
     · Introduces the initial Entity Framework migration, creating the TB_NombresPropios table in the Ciudadanos schema with columns for primary key, name, creation date, and user ID.

---
+++ CiudadanosServicesCS/Migrations/20251016180133_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CiudadanosServicesCS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Ciudadanos");
+
+            migrationBuilder.CreateTable(
+                name: "TB_NombresPropios",
+                schema: "Ciudadanos",
+                columns: table => new
+                {
+                    PkiNombrePropio = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FcNombrePropio = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false),
+                    FdFechaCreacion = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                    FiUsuarioCreacion = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_NombresPropios", x => x.PkiNombrePropio);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_NombresPropios",
+                schema: "Ciudadanos");
+        }
+    }
+}
