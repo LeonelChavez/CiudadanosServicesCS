b114f2d0bfd173e03276f4329f76fcb2b53019ee
Jack Leo <leonel.chavez@me.com>
2025-10-16 11:53:27 -0600

    Add Entity Framework Core and SQL Server setup
    Configured ApplicationDbContext with SQL Server using connection string from configuration. Registered controllers in the service container to support API endpoints.

--- CiudadanosServicesCS/Program.cs
+++ CiudadanosServicesCS/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using CiudadanosServicesCS.NombresPropios.Data; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var dbConectionString = builder.Configuration.GetConnectionString("ConexionSql");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(dbConectionString));
+    
+builder.Services.AddControllers();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
