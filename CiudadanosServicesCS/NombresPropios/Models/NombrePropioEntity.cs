2262660dcce3b5871f397207f892f9da68982f51
Jack Leo <leonel.chavez@me.com>
2025-10-16 11:32:35 -0600

    first commit

---
+++ CiudadanosServicesCS/NombresPropios/Models/NombrePropioEntity.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CiudadanosServicesCS.NombresPropios.Models
+{
+    [Table("tb_NombresPropios", Schema = "Ciudadanos")]
+    public class NombrePropioEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("PkiNombrePropio")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("FcNombrePropio", TypeName = "varchar(60)")]
+        [MaxLength(60)]
+        public string NombrePropio { get; set; } = null!;
+
+        [Required]
+        [Column("FdFechaCreacion", TypeName = "datetime2(0)")]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        [Column("FiUsuarioCreacion")]
+        public int UsuarioCreacion { get; set; }
+    }
+}
