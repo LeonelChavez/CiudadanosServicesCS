e0f1172413c8cd64ce27a13d49bdbc9f05f1c5ba
Jack Leo <leonel.chavez@me.com>
2025-10-16 11:47:49 -0600

    Update table name in NombrePropioEntity attribute
    Changed the table name in the Table attribute from 'tb_NombresPropios' to 'TB_NombresPropios' to match the correct casing or database convention.

--- CiudadanosServicesCS/NombresPropios/Models/NombrePropioEntity.cs
+++ CiudadanosServicesCS/NombresPropios/Models/NombrePropioEntity.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CiudadanosServicesCS.NombresPropios.Models
 {
-    [Table("tb_NombresPropios", Schema = "Ciudadanos")]
+    [Table("TB_NombresPropios", Schema = "Ciudadanos")]
     public class NombrePropioEntity
     {
         [Key]
